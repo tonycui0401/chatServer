a55aa1605da9a012c1e5f5b4f82af06c72950c3f
sosure0401 <cheng@so-sure.com>
2021-06-30 02:41:12 +0100

    remove add member

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const io = require("socket.io")(server);
 const cors = require("cors");
 const moment = require('moment');
 
-const {local_endpoint, auth_endpoint, remote_endpoint, local_base} = require('./configs/config');
+const { local_endpoint, auth_endpoint, remote_endpoint, local_base } = require('./configs/config');
 
 const fetch = require('node-fetch');
 
@@ -26,33 +26,33 @@ var users = {}
 var member = {}
 var generateMessage = (from, room) => {
   return {
-      from,
-      room,
-      // user,
-      createdDate: moment().valueOf()
+    from,
+    room,
+    // user,
+    createdDate: moment().valueOf()
   }
 };
 
 var generateUserMessage = (from, room, location, type, text) => {
   return {
-      from,
-      room,
-      location:location,
-      type,
-      text:text,
-      createdDate: moment().valueOf()
+    from,
+    room,
+    location: location,
+    type,
+    text: text,
+    createdDate: moment().valueOf()
   }
 };
 
 
 
-io.on("connection", function(client) {
+io.on("connection", function (client) {
   client.on("sign-in", e => {
     let user_id = e.user_id;
 
-    io.emit("online", {user_id:user_id});
+    io.emit("online", { user_id: user_id });
 
-  
+
 
 
 
@@ -67,13 +67,13 @@ io.on("connection", function(client) {
     }
 
 
-    fetch(local_endpoint+'/updateUserChatStatus?id='+client.user_id +'&chatstatus=online'
-    , {
-      method: 'put',
-      headers: { 'Content-Type': 'application/json' },
-  })
-  .then(res => res.json())
-  .then(json => console.log(json));
+    fetch(local_endpoint + '/updateUserChatStatus?id=' + client.user_id + '&chatstatus=online'
+      , {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+      })
+      .then(res => res.json())
+      .then(json => console.log(json));
 
 
   });
@@ -84,220 +84,220 @@ io.on("connection", function(client) {
 
 
     client.join(params.room_id);
-  
-    users[params.room_id]={}
-    member[params.room_id]={}
 
-    if(!users[params.room_id].users){
-      users[params.room_id].users=[]
+    users[params.room_id] = {}
+    member[params.room_id] = {}
+
+    if (!users[params.room_id].users) {
+      users[params.room_id].users = []
     }
 
-    if(!member[params.room_id].users){
-      member[params.room_id].users=[]
+    if (!member[params.room_id].users) {
+      member[params.room_id].users = []
     }
 
-    fetch(local_endpoint+'/allChatGroupMembers?room='+params.room_id)
-    .then(res => res.json())
-    .then(json => {
-      
-      console.log("check members")
-      console.log(json)
+    fetch(local_endpoint + '/allChatGroupMembers?room=' + params.room_id)
+      .then(res => res.json())
+      .then(json => {
 
-     
-        users[params.room_id].users=json
+        console.log("check members")
+        console.log(json)
+
+
+        users[params.room_id].users = json
         console.log("print me out")
-        for (let i in json){
-console.log("print me")
-        console.log(json[i])
-        member[params.room_id].users.push(json[i].member)
+        for (let i in json) {
+          console.log("print me")
+          console.log(json[i])
+          member[params.room_id].users.push(json[i].member)
 
-      }
-
-
-      console.log(member[params.room_id].users.includes(params.user_id))
-
-      console.log("end check members")
+        }
 
 
+        console.log(member[params.room_id].users.includes(params.user_id))
 
-      if(!member[params.room_id].users.includes(params.user_id)){
-    
-        let newUser = {
-          user_id: params.user_id,
-          firstname: params.firstname,
-          lastname: params.lastname,
-          img: params.img,
-          joinDate: moment().valueOf()
-      }
-  
-  
-      fetch(local_endpoint+'/createChatGroupMember'
-      , {
-        method: 'post',
-        body:    JSON.stringify({
-          room:params.room_id,
-          member:params.user_id,
-          joindate:moment().valueOf(),
-          membertype:'normal'
-        }),
-        headers: { 'Content-Type': 'application/json' },
-    })
-    .then(res => res.json())
-    .then(json => console.log(json));
-      
-  
-  
-      users[params.room_id].users.push(newUser)
-    
-      
-    
-  
-      }
-  
-      client.emit('newMessage', generateMessage(params.user_id, params.room_id));
-    
-      client.broadcast.to(params.room_id).emit('newMessage', generateMessage(params.user_id, params.room_id));
-  
+        console.log("end check members")
 
 
-    
-    })
 
-  
-   
-   console.log(member[params.room_id].users.includes(params.user_id))
-   
+        //   if(!member[params.room_id].users.includes(params.user_id)){
 
-});
+        //     let newUser = {
+        //       user_id: params.user_id,
+        //       firstname: params.firstname,
+        //       lastname: params.lastname,
+        //       img: params.img,
+        //       joinDate: moment().valueOf()
+        //   }
 
 
-client.on('createMessage', (message) => {
+        //   fetch(local_endpoint+'/createChatGroupMember'
+        //   , {
+        //     method: 'post',
+        //     body:    JSON.stringify({
+        //       room:params.room_id,
+        //       member:params.user_id,
+        //       joindate:moment().valueOf(),
+        //       membertype:'normal'
+        //     }),
+        //     headers: { 'Content-Type': 'application/json' },
+        // })
+        // .then(res => res.json())
+        // .then(json => console.log(json));
+
+
+
+        //   users[params.room_id].users.push(newUser)
+
+
+
+
+        //   }
+
+        client.emit('newMessage', generateMessage(params.user_id, params.room_id));
+
+        client.broadcast.to(params.room_id).emit('newMessage', generateMessage(params.user_id, params.room_id));
+
+
+
+
+      })
+
+
+
+    console.log(member[params.room_id].users.includes(params.user_id))
+
+
+  });
+
+
+  client.on('createMessage', (message) => {
 
     console.log("on create new messages")
     console.log(message)
-      let tempObj = generateUserMessage(message.user_id, message.room, 'in', message.type, message.text);
-      io.to(message.room).emit('newGroupMessage', tempObj);
+    let tempObj = generateUserMessage(message.user_id, message.room, 'in', message.type, message.text);
+    io.to(message.room).emit('newGroupMessage', tempObj);
 
 
 
 
-  fetch(local_endpoint+'/createChatGroupMsg'
-  , {
-    method: 'post',
-    body:    JSON.stringify({
-      room:message.room,
-      sender:message.user_id,
-      message:message.text,
-      time:moment().valueOf(),
-      type:message.type
-    }),
-    headers: { 'Content-Type': 'application/json' },
-})
-.then(res => res.json())
-.then(mjson => {
-  
-  console.log("test group id")
-  console.log(mjson.id)
-  console.log("end test group id")
-
-//   fetch(local_endpoint+'/createChatGroupMsgStatus'
-//   , {
-//     method: 'post',
-//     body:    JSON.stringify({
-//       msg_id:mjson.id,
-//       seenby:message.user_id,
-//       seenat:'seen'
-//     }),
-//     headers: { 'Content-Type': 'application/json' },
-// }).then(res => res.json())
-// .then(json => console.log(json));
-
-console.log("end test seen user id")
-
-  fetch(local_endpoint+'/updateGroupLastChannel'
-  , {
-    method: 'put',
-    body:    JSON.stringify({
-      group_id:message.room,
-      sender:message.user_id,
-      message:message.text,
-      time:moment().valueOf(),
-      type:message.type,
-      message_id:mjson.id
-    }),
-    headers: { 'Content-Type': 'application/json' },
-})
-.then(res => res.json())
-.then(json => console.log(json));
-
-
-
-  fetch(local_endpoint+'/allChatGroupMembers?room='+message.room)
-.then(res => res.json())
-.then(json => {
-
-
-
-  
-    for (let i in json){
-
-      if (json[i].member === message.user_id) {
-
-       // continue;
-
-       fetch(local_endpoint+'/createChatGroupMsgStatus'
-       , {
-         method: 'post',
-         body:    JSON.stringify({
-           msg_id:mjson.id,
-           seenby:json[i].member,
-           seenat:moment().valueOf(),
-           status:'seen'
-         }),
-         headers: { 'Content-Type': 'application/json' },
-     })
-     .then(res => res.json())
-     .then(json => console.log(json));
- 
-
-      }else{
-
-
-      fetch(auth_endpoint+'/sendIosNotification?id='+json[i].member
-      , {
-        method: 'get',
-        headers: { 'Content-Type': 'application/json' },
-    })
-    .then(res => res.json())
-    .then(json => console.log(json));
-
-
-
-
-      fetch(local_endpoint+'/createChatGroupMsgStatus'
+    fetch(local_endpoint + '/createChatGroupMsg'
       , {
         method: 'post',
-        body:    JSON.stringify({
-          msg_id:mjson.id,
-          seenby:json[i].member,
-          seenat:null,
-          status:'unseen'
+        body: JSON.stringify({
+          room: message.room,
+          sender: message.user_id,
+          message: message.text,
+          time: moment().valueOf(),
+          type: message.type
         }),
         headers: { 'Content-Type': 'application/json' },
-    })
-    .then(res => res.json())
-    .then(json => console.log(json));
+      })
+      .then(res => res.json())
+      .then(mjson => {
 
-  }
-  }
+        console.log("test group id")
+        console.log(mjson.id)
+        console.log("end test group id")
+
+        //   fetch(local_endpoint+'/createChatGroupMsgStatus'
+        //   , {
+        //     method: 'post',
+        //     body:    JSON.stringify({
+        //       msg_id:mjson.id,
+        //       seenby:message.user_id,
+        //       seenat:'seen'
+        //     }),
+        //     headers: { 'Content-Type': 'application/json' },
+        // }).then(res => res.json())
+        // .then(json => console.log(json));
+
+        console.log("end test seen user id")
+
+        fetch(local_endpoint + '/updateGroupLastChannel'
+          , {
+            method: 'put',
+            body: JSON.stringify({
+              group_id: message.room,
+              sender: message.user_id,
+              message: message.text,
+              time: moment().valueOf(),
+              type: message.type,
+              message_id: mjson.id
+            }),
+            headers: { 'Content-Type': 'application/json' },
+          })
+          .then(res => res.json())
+          .then(json => console.log(json));
 
 
-});
+
+        fetch(local_endpoint + '/allChatGroupMembers?room=' + message.room)
+          .then(res => res.json())
+          .then(json => {
 
 
 
 
-});
+            for (let i in json) {
+
+              if (json[i].member === message.user_id) {
+
+                // continue;
+
+                fetch(local_endpoint + '/createChatGroupMsgStatus'
+                  , {
+                    method: 'post',
+                    body: JSON.stringify({
+                      msg_id: mjson.id,
+                      seenby: json[i].member,
+                      seenat: moment().valueOf(),
+                      status: 'seen'
+                    }),
+                    headers: { 'Content-Type': 'application/json' },
+                  })
+                  .then(res => res.json())
+                  .then(json => console.log(json));
+
+
+              } else {
+
+
+                fetch(auth_endpoint + '/sendIosNotification?id=' + json[i].member
+                  , {
+                    method: 'get',
+                    headers: { 'Content-Type': 'application/json' },
+                  })
+                  .then(res => res.json())
+                  .then(json => console.log(json));
+
+
+
+
+                fetch(local_endpoint + '/createChatGroupMsgStatus'
+                  , {
+                    method: 'post',
+                    body: JSON.stringify({
+                      msg_id: mjson.id,
+                      seenby: json[i].member,
+                      seenat: null,
+                      status: 'unseen'
+                    }),
+                    headers: { 'Content-Type': 'application/json' },
+                  })
+                  .then(res => res.json())
+                  .then(json => console.log(json));
+
+              }
+            }
+
+
+          });
+
+
+
+
+      });
 
 
 
@@ -309,10 +309,10 @@ console.log("end test seen user id")
 
 
 
-  console.log("get client id")
+    console.log("get client id")
 
 
-});
+  });
 
 
 
@@ -324,95 +324,93 @@ console.log("end test seen user id")
     console.log(targetId)
     console.log(clients[targetId])
     console.log(sourceId)
-    if(targetId && clients[targetId]) {
+    if (targetId && clients[targetId]) {
       clients[targetId].forEach(cli => {
 
 
 
-     
+
 
 
 
         cli.emit("message", e);
 
-       
+
 
 
         console.log("emited this")
       });
     }
 
-    if(sourceId && clients[sourceId]) {
+    if (sourceId && clients[sourceId]) {
       clients[sourceId].forEach(cli => {
 
 
 
-     
+
 
         cli.emit("message", e);
 
-        
+
         console.log("emited that")
       });
     }
 
 
 
-    fetch(auth_endpoint+'/sendIosNotification?id='+e.to
-    , {
-      method: 'get',
-      headers: { 'Content-Type': 'application/json' },
-  })
-  .then(res => res.json())
-  .then(json => console.log(json));
+    fetch(auth_endpoint + '/sendIosNotification?id=' + e.to
+      , {
+        method: 'get',
+        headers: { 'Content-Type': 'application/json' },
+      })
+      .then(res => res.json())
+      .then(json => console.log(json));
 
 
 
-    fetch(local_endpoint+'/createPrivateChat'
-    , {
-      method: 'post',
-      body:    JSON.stringify({
-        sender:e.from,
-        receipt:e.to,
-        message:e.message.message,
-        time:e.message.time,
-        type:e.message.type
-      }),
-      headers: { 'Content-Type': 'application/json' },
-  })
-  .then(res => res.json())
-  .then(json => {
-    
-    
-    // console.log(json)
+    fetch(local_endpoint + '/createPrivateChat'
+      , {
+        method: 'post',
+        body: JSON.stringify({
+          sender: e.from,
+          receipt: e.to,
+          message: e.message.message,
+          time: e.message.time,
+          type: e.message.type
+        }),
+        headers: { 'Content-Type': 'application/json' },
+      })
+      .then(res => res.json())
+      .then(json => {
 
-    // console.log(local_endpoint+'/updatePrivateLastChannel?sender='+e.from+'&receipt='+e.to+'&message='+e.message.message+'&time='+e.message.time+'&type='+e.message.type+'&message_id='+json.id)
-  
-    fetch(local_endpoint+'/updatePrivateLastChannel'
-    , {
-      method: 'put',
-      body:    JSON.stringify({
-        sender:e.from,
-        receipt:e.to,
-        message:e.message.message,
-        time:e.message.time,
-        type:e.message.type,
-        message_id:json.id
-      }),
-      headers: { 'Content-Type': 'application/json' },
-  })
-  .then(res => res.json())
-  .then(json => console.log(json));
-  
-  
+
+        // console.log(json)
+
+        // console.log(local_endpoint+'/updatePrivateLastChannel?sender='+e.from+'&receipt='+e.to+'&message='+e.message.message+'&time='+e.message.time+'&type='+e.message.type+'&message_id='+json.id)
+
+        fetch(local_endpoint + '/updatePrivateLastChannel'
+          , {
+            method: 'put',
+            body: JSON.stringify({
+              sender: e.from,
+              receipt: e.to,
+              message: e.message.message,
+              time: e.message.time,
+              type: e.message.type,
+              message_id: json.id
+            }),
+            headers: { 'Content-Type': 'application/json' },
+          })
+          .then(res => res.json())
+          .then(json => console.log(json));
+
+
+      });
+
+
+
+
   });
-    
-
-
-
-  });
-
- 
 
 
 
@@ -425,20 +423,22 @@ console.log("end test seen user id")
 
 
 
-  client.on("disconnect", id=> {
+
+
+  client.on("disconnect", id => {
 
     let targetId = 78;
     let sourceId = 77;
-  io.emit("offline", {user_id:client.user_id});
+    io.emit("offline", { user_id: client.user_id });
 
 
-  fetch(local_endpoint+'/updateUserChatStatus?id='+client.user_id+'&chatstatus=offline'
-  , {
-    method: 'put',
-    headers: { 'Content-Type': 'application/json' },
-})
-.then(res => res.json())
-.then(json => console.log(json));
+    fetch(local_endpoint + '/updateUserChatStatus?id=' + client.user_id + '&chatstatus=offline'
+      , {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+      })
+      .then(res => res.json())
+      .then(json => console.log(json));
 
 
 
@@ -467,11 +467,11 @@ app.get('/users', (req, res) => {
   fetch('http://localhost:5000/api/tags/locationTags')
     .then(res => res.json())
     .then(json => {
-      
+
       console.log(json)
 
       res.send(json);
-    
+
     });
 
 
@@ -486,7 +486,7 @@ app.get('/sign_s3_chat_group_image', (req, res) => {
   const room_id = req.query.room_id;
 
 
-io.emit("new group image message", {url:fileurl, user_id:user_id, room_id:room_id, fileext:fileext});
+  io.emit("new group image message", { url: fileurl, user_id: user_id, room_id: room_id, fileext: fileext });
 
 });
 
@@ -500,10 +500,10 @@ app.get('/sign_s3_chat_image', (req, res) => {
   const from_id = req.query.from_id;
 
 
-    io.emit("new image message", {url:fileurl, user_id:user_id, from_id:from_id, fileext:fileext});
+  io.emit("new image message", { url: fileurl, user_id: user_id, from_id: from_id, fileext: fileext });
 
 
-    
+
 
 
 });
